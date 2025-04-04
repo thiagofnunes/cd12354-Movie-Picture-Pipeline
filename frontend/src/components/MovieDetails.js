6f732fe242c33b73b3cc9cd0e7d8440fd8d137f5
Thiago Nunes <thiago.nunes@ctw.bmwgroup.com>
2025-04-04 16:07:53 +0100

    Revert "Minor fix"
    This reverts commit 4babe00e862a2544dc0a6ad35ab92bf39501fabd.

--- frontend/src/components/MovieDetails.js
+++ frontend/src/components/MovieDetails.js
@@ -4,11 +4,7 @@ import axios from 'axios';
 function MovieDetail({ movie }) {
   const [details, setDetails] = useState(null);
   useEffect(() => {
-    if (!process.env.REACT_APP_MOVIE_API_URL) {
-      process.env.REACT_APP_MOVIE_API_URL = 'a788a22c14d34488a87b89402a405c03-2092943536.us-east-1.elb.amazonaws.com';
-    }
-    
-    axios.get(`${process.env.REACT_APP_MOVIE_API_URL}/movies/${movie.id}`).then((response) => {
+    axios.get(`a788a22c14d34488a87b89402a405c03-2092943536.us-east-1.elb.amazonaws.com/movies/${movie.id}`).then((response) => {
       setDetails(response.data);
     });
   }, [movie]);
--- frontend/src/components/MovieList.js
+++ frontend/src/components/MovieList.js
@@ -1,33 +1,29 @@
-import React, {useEffect, useState} from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import axios from 'axios';
 
-function MovieList({onMovieClick}) {
-    const [movies, setMovies] = useState([]);
+function MovieList({ onMovieClick }) {
+  const [movies, setMovies] = useState([]);
 
-    if (!process.env.REACT_APP_MOVIE_API_URL) {
-        process.env.REACT_APP_MOVIE_API_URL = 'a788a22c14d34488a87b89402a405c03-2092943536.us-east-1.elb.amazonaws.com';
-    }
+  useEffect(() => {
+    axios.get(`a788a22c14d34488a87b89402a405c03-2092943536.us-east-1.elb.amazonaws.com/movies`).then((response) => {
+      setMovies(response.data.movies);
+    });
+  }, []);
 
-    useEffect(() => {
-        axios.get(`${process.env.REACT_APP_MOVIE_API_URL}/movies`).then((response) => {
-            setMovies(response.data.movies);
-        });
-    }, []);
-
-    return (
-        <ul>
-            {movies.map((movie) => (
-                <li className="movieItem" key={movie.id} onClick={() => onMovieClick(movie)}>
-                    {movie.title}
-                </li>
-            ))}
-        </ul>
-    );
+  return (
+    <ul>
+      {movies.map((movie) => (
+        <li className="movieItem" key={movie.id} onClick={() => onMovieClick(movie)}>
+          {movie.title}
+        </li>
+      ))}
+    </ul>
+  );
 }
 
 MovieList.propTypes = {
-    onMovieClick: PropTypes.func.isRequired,
+  onMovieClick: PropTypes.func.isRequired,
 };
 
 export default MovieList;
